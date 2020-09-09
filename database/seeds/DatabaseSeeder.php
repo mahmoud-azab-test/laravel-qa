76b644e0f8ae4d23beeecfdef092316ac988d66f
mahmoud-azab-mlvp <mahmoud.azab.mlvp@gmail.com>
2020-09-09 17:30:15 +0200

    prepuplate users and questions tables using model factroies and seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Database\Seeder;
 
+use Illuminate\Database\Seeder;
+use App\Question;
+use App\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(User::class, 3)->create()->each(function($u){
+            $u->questions()->saveMany(factory(Question::class, rand(1, 5))->make());
+        });
+        //factory(Question::class, 10)->create();
     }
 }
